176e5cc02d4cb760cd9bdabf70eb6f875807dcf8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-01-31 19:21:11 +0100

    can upload to arduino nano

--- library/set_environment_variables.bat
+++ library/set_environment_variables.bat
@@ -15,16 +15,19 @@ SET ARDUINO_MCU=atmega328p
 SET ARDUINO_VARIANT=eightanaloginputs
 
 @rem the name of the programmer you wish to use (usually stk500)
-SET ARDUINO_PROGRAMMER=avrisp2
+SET ARDUINO_PROGRAMMER=arduino
 
 @rem the clock frequency of your microcontroller (usually 16000000 for atmega168)
 SET ARDUINO_FCPU=16000000
 
 @rem the port to which your programmer is connected (e.g. COM1, COM2, etc.)
-SET ARDUINO_COMPORT=COM24
+SET ARDUINO_COMPORT=COM23
 
 @rem the baud rate at which the download is to occur (19200 seems to be a good starting point)
-SET ARDUINO_BURNRATE=19200
+rem according to http://arduino.cc/en/Serial/Begin these are possible baud rates
+rem 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, or 115200
+rem see baud rates here http://42bots.com/resources/arduino-program-sketch-upload-speeds/
+SET ARDUINO_BURNRATE=57600
 
 @rem The directory for user defined libraries
 SET ARDUINO_USER_LIBRARIES=C:\Users\cheche\Documents\Arduino\libraries
