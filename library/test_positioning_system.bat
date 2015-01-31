945176d7717c98edb655a4092f7162dbdb4b19e0
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-01-31 19:30:26 +0100

    added message if siye is too big

--- library/test_positioning_system.bat
+++ library/test_positioning_system.bat
@@ -8,5 +8,12 @@ call set_environment_variables.bat
 
 call abuild.bat -r -c "positioning_system\examples\test_solver\test_solver.pde" 
 
-call aupload.bat obj/test_solver.elf
+FOR %%A IN (obj\*.elf) DO set elf_file_size=%%~zA
+
+rem comparing http://stackoverflow.com/questions/9278614/if-greater-than-batch-files
+if !elf_file_size! LEQ  30720 (
+  call aupload.bat obj/test_solver.elf
+) else (
+  echo !elf_file_size! is too big for the arduino. 30720 is allowed.
+)
 
