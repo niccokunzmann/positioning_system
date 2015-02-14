01a26567a41c0591dc24fc18ccdf13f03edee565
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-02-14 20:45:43 +0100

    can solve equations of order 4

--- library/positioning_system_test/examples/test_equation_solving_of_order_4/test_equation_solving_of_order_4.ino
+++ library/positioning_system_test/examples/test_equation_solving_of_order_4/test_equation_solving_of_order_4.ino
@@ -20,12 +20,13 @@ test(equation_4_q_is_0) {
   solve_equation(a, b, c, d, e, &zero1, &zero2, &zero3, &zero4);
   sort_numbers(&zero1, &zero2, &zero3, &zero4);
   print_zeros();
-  assertEqual(zero1, 41);
-  assertEqual(zero2, 42);
-  assertEqual(zero3, 43);
-  assertEqual(zero4, 44);
+  
+  assertApproximately(zero1, 41.);
+  assertApproximately(zero2, 42.);
+  assertApproximately(zero3, 43.);
+  assertApproximately(zero4, 44.);
 }
-/*
+
 test(equation_3_for_equation_4) {
   b = 8;
   c = 17.5;
@@ -34,25 +35,24 @@ test(equation_3_for_equation_4) {
   solve_equation(b, c, d, e, &zero1, &zero2, &zero3);
   sort_numbers(&zero1, &zero2, &zero3);
   
+  print_zeros();
   assertApproximately(zero1, -2.0625);
   assertApproximately(zero2, -1.0625);
   assertApproximately(zero3, 0.9375);
-
-  assertEqual(call(0, b, c, d, e, zero1), 0);
-  assertEqual(call(0, b, c, d, e, zero2), 0);
-  assertEqual(call(0, b, c, d, e, zero3), 0);
 }
 
 test(equation_4) {
   get_coefficients_from_zeros(40, 42, 43, 44, &a, &b, &c, &d, &e);
   solve_equation(a, b, c, d, e, &zero1, &zero2, &zero3, &zero4);
   sort_numbers(&zero1, &zero2, &zero3, &zero4);
-  assertEqual(zero1, 40);
-  assertEqual(zero2, 42);
-  assertEqual(zero3, 43);
-  assertEqual(zero4, 44);
+  
+  print_zeros();
+  assertApproximately(zero1, 40.);
+  assertApproximately(zero2, 42.);
+  assertApproximately(zero3, 43.);
+  assertApproximately(zero4, 44.);
 }
-*/
+
 void setup() {
   Serial.begin(9600);
 }
